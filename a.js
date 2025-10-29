929d50b10ccec1aa4828aefea7732d743d3aaaf4
Dechen Wangmo <wangmod066@gmail.com>
2025-10-30 00:32:17 +0600

    lastcommit

--- a.js
+++
@@ -1,8 +0,0 @@
-<script>
-  const hamburger = document.getElementById('hamburger');
-  const navLinks = document.querySelector('.nav-links');
-
-  hamburger.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-  });
-</script>
